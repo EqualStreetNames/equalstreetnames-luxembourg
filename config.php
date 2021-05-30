8c0c085eca9c9c6157a08dc72e28d0d588ba7e2b
Jonathan Beliën <jbelien@users.noreply.github.com>
2021-05-30 20:44:29 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     // Relation ID of the OpenstreetMap City
-    'relationId' => 0,
+    'relationId' => 407489,
 
     // Languages in which you want to extract Wiki informations
     'languages' => ['en'],
